451ee2f5b306bb7a209c4792651605276c916891
sergtix <47977510+sergtix@users.noreply.github.com>
2019-06-04 16:12:18 +0300

    Add files via upload

---
+++ converter.js
@@ -0,0 +1,4 @@
+var celsius = +prompt("Введите температуру в градусах по Цельсию.");
+var fahrenheit = (9/5)*celsius+32;
+
+alert(celsius + " градусов по Цельсию = " + fahrenheit + " градусов по Фаренгейту.");
